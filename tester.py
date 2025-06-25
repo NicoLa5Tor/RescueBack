ffedacc3bc940d897dcb63d6d3bc8ec270a66c6d
NicoLa5Tor <nico.rodriguez.torres.71@gmail.com>
2025-06-25 18:28:50 -0500

    sedes en empresa

--- tester.py
+++ tester.py
@@ -1,8 +1,8 @@
 from endpoint_test_client import EndpointTestClient
 empresa_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTc1MDczNzY3MSwianRpIjoiZWVhMzgzMzktNTRjNy00OTgxLTk4ZGEtNmJjODJmM2RmMzk0IiwidHlwZSI6ImFjY2VzcyIsInN1YiI6IjY4NThjZTAzYjA0M2I1YzdkOGVjYWE0MCIsIm5iZiI6MTc1MDczNzY3MSwiY3NyZiI6ImRiOTU5NzlhLTllNzQtNDYyMC05Mjk4LTE2MjQ0OTI0NmI4MSIsImV4cCI6MTc1MDczODU3MSwiZW1haWwiOiIxQDIuY29tIiwidXNlcm5hbWUiOiJlbXByZXNhIiwicm9sZSI6ImVtcHJlc2EiLCJwZXJtaXNvcyI6WyIvYXBpL2VtcHJlc2FzIiwiL2VtcHJlc2FzIl19.xwdYRCJh1zYdRXOWK25X-Db5Yy8-HELAfP3hRzygfRk"
-admin_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTc1MDgyMzY1NCwianRpIjoiNGYwMzlkOTgtYzllYS00YTRlLWI2N2EtM2M4MzM1YzBhMjU2IiwidHlwZSI6ImFjY2VzcyIsInN1YiI6IjY4NGNiNGRkNTRiNDZiNmZhMmM1OWYzOSIsIm5iZiI6MTc1MDgyMzY1NCwiY3NyZiI6IjZkNGI3MmZhLTM0YjAtNGUwZi1iNGQ1LWRkYzQ5NjZiODBiYiIsImV4cCI6MTc1MDgyNDU1NCwiZW1haWwiOiJhZG1pbkBleGFtcGxlLmNvbSIsInVzZXJuYW1lIjoiYWRtaW4wMSIsInJvbGUiOiJzdXBlcl9hZG1pbiIsInBlcm1pc29zIjpbIi9hcGkvdXNlcnMiLCIvYXBpL2VtcHJlc2FzIiwiL2FwaS9hZG1pbiIsIi9lbXByZXNhcyJdfQ.CtNU5uayrUCQGX4o3lE81hN7kb4h_pF-_UJsOrT_mdo"
+admin_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTc1MDg3NTUwNywianRpIjoiNTEyMmYyNDYtMGRiNy00YWZlLTk4N2ItYjA2MDNhNjVjMDVkIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6IjY4NGNiNGRkNTRiNDZiNmZhMmM1OWYzOSIsIm5iZiI6MTc1MDg3NTUwNywiY3NyZiI6ImU1M2E4ODU1LThlN2EtNDMxZS05NDJjLTgxMjVlZjhhNDc0YiIsImV4cCI6MTc1MDg3NjQwNywiZW1haWwiOiJhZG1pbkBleGFtcGxlLmNvbSIsInVzZXJuYW1lIjoiYWRtaW4wMSIsInJvbGUiOiJzdXBlcl9hZG1pbiIsInBlcm1pc29zIjpbIi9hcGkvdXNlcnMiLCIvYXBpL2VtcHJlc2FzIiwiL2FwaS9hZG1pbiIsIi9lbXByZXNhcyJdfQ.Y8by7gGolb0TWig_ezurCLwZ053lmbzFb7DL3-Rkams"
 obj = EndpointTestClient(token=admin_token)
-#region useuarios
+#region usuarios
 def getUsuarios():
   print(obj.get_users().json())
 def getUser():
@@ -17,6 +17,19 @@ def createUsuario():
         }
    response = obj.create_user(data=data)
    print(response.json())
+#region Empresa
+def creatEmpresa():
+  data = {
+            "nombre": "Mi Empresa 3",
+            "descripcion": "Empresa de ejemplo",
+            "ubicacion": "Bogotá",
+            "sedes": ["Principal","secundaria"],
+            "username": "nicolaspruebas",
+            "email": "empresa2@example.com",
+            "password": "secreto"
+        }
+  print(obj.create_empresa(data=data).json())
+  
 def getEmpresas():
   print(obj.get_empresas().json())
 def getEmpresasActivity():
@@ -32,4 +45,4 @@ def createBotonera():
   }
   print(obj.create_botonera(data=data).json())
 
-createBotonera()
+creatEmpresa()
